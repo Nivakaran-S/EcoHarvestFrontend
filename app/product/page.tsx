8efd4c5015b8beceedc04b27b74551032612f720
sliitguy <it23259416@my.sliit.lk>
2025-08-28 19:50:50 +0530

    updated console log product details

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -82,7 +82,7 @@ const ProductPageComponent = () => {
           ...response.data,
           imageUrl: response.data.imageUrl || ProductImage2.src,
         });
-        console.log('Product details', response)
+        console.log('Product details', productDetails?.name)
       } catch (err) {
         console.error("Error fetching product details:", err);
         setError("Failed to load product details");
